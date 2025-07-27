444864c6b847fc7a4a1afb6132752951f7b07fc8
Pablo Alves <pabloalves1213@gmail.com>
2025-07-27 20:37:26 -0300

    atualizacao27/07

---
+++ script.js
@@ -0,0 +1,103 @@
+function quiz(){
+
+const questions = [
+    {
+        questions: "Qual é a capital do Brasil?",
+        answer: [
+            { id: A, Text: "São Paulo", correct: false },
+            { id: B, Text: "Rio de janeiro", correct: false },
+            { id: C, Text: "Brasilia", correct: true },
+            { id: D, Text: "Salvador", correct: false },
+        ]
+    }
+
+    {
+        questions: "Quem pintou a Mona Lisa?",
+        answer: [
+            { id: A, Text: "Vincent van Gogh", correct: false },
+            { id: B, Text: "Leonardo da Vinci ", correct: true },
+            { id: C, Text: "Pablo Picasso", correct: false },
+            { id: D, Text: "Michelangelo", correct: false },
+        ]
+    }
+    {
+        questions: "Qual planeta é conhecido como o Planeta Vermelho",
+        answer: [
+        { id: A, Text: "Júpiter", correct: false },
+        { id: B, Text: "Marte", correct: true },
+        { id: C, Text: "Vênus", correct: false },
+        { id: D, Text: "Saturno", correct: false },
+        ]
+    }
+
+    {
+        questions: "Qual tag HTML é usada para criar um parágrafo?",
+        answer: [
+        { id: A, Text: "<div>", correct: false },
+        { id: B, Text: "<span>", correct: false },
+        { id: C, Text: "<p>", correct: true },
+        { id: D, Text: "<text>", correct: false },
+        ]
+    }
+
+    {
+        questions: "Qual desses animais é um mamífero?",
+        answer: [
+        { id: A, Text: "Jacaré", correct: false },
+        { id: B, Text: "Tartaruga", correct: false },
+        { id: C, Text: "Golfinho", correct: true },
+        { id: D, Text: "Pinguim", correct: false },
+        ]
+    }
+
+      {
+        questions: "Quanto é 7 x 8?",
+        answer: [
+        { id: A, Text: "56", correct: true },
+        { id: B, Text: "64", correct: false },
+        { id: C, Text: "49", correct: false},
+        { id: D, Text: "58", correct: false },
+        ]
+    }
+
+      {
+        questions: "Qual dessas palavras é um verbo?",
+        answer: [
+        { id: A, Text: "Casa", correct: false },
+        { id: B, Text: "Rapído", correct: false },
+        { id: C, Text: "Correr", correct: true},
+        { id: D, Text: "Amarelo", correct: false },
+        ]
+    }
+
+       {
+        questions: "Quanto é a raiz quadrada de 144?",
+        answer: [
+        { id: A, Text: "11", correct: false },
+        { id: B, Text: "14", correct: false },
+        { id: C, Text: "12", correct: true},
+        { id: D, Text: "13", correct: false },
+        ]
+    }
+
+          {
+        questions: "Qual elemento químico tem o símbolo “O”?",
+        answer: [
+        { id: A, Text: "Ouro", correct: false },
+        { id: B, Text: "Oxigênio", correct: true},
+        { id: C, Text: "Osmônio", correct: true},
+        { id: D, Text: "Óxido", correct: false },
+        ]
+    }
+
+              {
+        questions: "", //pergunta zoeira// elabora
+        answer: [
+        { id: A, Text: "*", correct: false },
+        { id: B, Text: "Oxigênio", correct: true},
+        { id: C, Text: "Osmônio", correct: true},
+        { id: D, Text: "Óxido", correct: false },
+        ]
+    }
+    ]
+}
