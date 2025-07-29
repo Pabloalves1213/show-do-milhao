af3539e381e248f15b7bdc17fafc30bfe8baa6a8
Pablo Alves <pabloalves1213@gmail.com>
2025-07-28 22:38:22 -0300

    atualizando

--- script.js
+++ script.js
@@ -1,103 +1,174 @@
-function quiz(){
+
 
 const questions = [
+
     {
         questions: "Qual é a capital do Brasil?",
         answer: [
-            { id: A, Text: "São Paulo", correct: false },
-            { id: B, Text: "Rio de janeiro", correct: false },
-            { id: C, Text: "Brasilia", correct: true },
-            { id: D, Text: "Salvador", correct: false },
+            { id: 1, Text: "São Paulo", correct: false },
+            { id: 2, Text: "Rio de janeiro", correct: false },
+            { id: 3, Text: "Brasilia", correct: true },
+            { id: 4, Text: "Salvador", correct: false },
         ]
-    }
+    },
+
+
 
     {
         questions: "Quem pintou a Mona Lisa?",
         answer: [
-            { id: A, Text: "Vincent van Gogh", correct: false },
-            { id: B, Text: "Leonardo da Vinci ", correct: true },
-            { id: C, Text: "Pablo Picasso", correct: false },
-            { id: D, Text: "Michelangelo", correct: false },
+            { id: 1, Text: "Vincent van Gogh", correct: false },
+            { id: 2, Text: "Leonardo da Vinci ", correct: true },
+            { id: 3, Text: "Pablo Picasso", correct: false },
+            { id: 4, Text: "Michelangelo", correct: false },
         ]
-    }
+    },
+
+
     {
         questions: "Qual planeta é conhecido como o Planeta Vermelho",
         answer: [
-        { id: A, Text: "Júpiter", correct: false },
-        { id: B, Text: "Marte", correct: true },
-        { id: C, Text: "Vênus", correct: false },
-        { id: D, Text: "Saturno", correct: false },
+            { id: 1, Text: "Júpiter", correct: false },
+            { id: 2, Text: "Marte", correct: true },
+            { id: 3, Text: "Vênus", correct: false },
+            { id: 4, Text: "Saturno", correct: false },
         ]
-    }
+    },
+
+
 
     {
         questions: "Qual tag HTML é usada para criar um parágrafo?",
         answer: [
-        { id: A, Text: "<div>", correct: false },
-        { id: B, Text: "<span>", correct: false },
-        { id: C, Text: "<p>", correct: true },
-        { id: D, Text: "<text>", correct: false },
+            { id: 1, Text: "<div>", correct: false },
+            { id: 2, Text: "<span>", correct: false },
+            { id: 3, Text: "<p>", correct: true },
+            { id: 4, Text: "<text>", correct: false },
         ]
-    }
+    },
+
+
 
     {
         questions: "Qual desses animais é um mamífero?",
         answer: [
-        { id: A, Text: "Jacaré", correct: false },
-        { id: B, Text: "Tartaruga", correct: false },
-        { id: C, Text: "Golfinho", correct: true },
-        { id: D, Text: "Pinguim", correct: false },
+            { id: 1, Text: "Jacaré", correct: false },
+            { id: 2, Text: "Tartaruga", correct: false },
+            { id: 3, Text: "Golfinho", correct: true },
+            { id: 4, Text: "Pinguim", correct: false },
         ]
-    }
+    },
 
-      {
+
+    {
         questions: "Quanto é 7 x 8?",
         answer: [
-        { id: A, Text: "56", correct: true },
-        { id: B, Text: "64", correct: false },
-        { id: C, Text: "49", correct: false},
-        { id: D, Text: "58", correct: false },
+            { id: 1, Text: "56", correct: true },
+            { id: 2, Text: "64", correct: false },
+            { id: 3, Text: "49", correct: false },
+            { id: 4, Text: "58", correct: false },
         ]
-    }
+    },
 
-      {
+
+    {
         questions: "Qual dessas palavras é um verbo?",
         answer: [
-        { id: A, Text: "Casa", correct: false },
-        { id: B, Text: "Rapído", correct: false },
-        { id: C, Text: "Correr", correct: true},
-        { id: D, Text: "Amarelo", correct: false },
+            { id: 1, Text: "Casa", correct: false },
+            { id: 2, Text: "Rapído", correct: false },
+            { id: 3, Text: "Correr", correct: true },
+            { id: 4, Text: "Amarelo", correct: false },
         ]
-    }
+    },
 
-       {
+
+    {
         questions: "Quanto é a raiz quadrada de 144?",
         answer: [
-        { id: A, Text: "11", correct: false },
-        { id: B, Text: "14", correct: false },
-        { id: C, Text: "12", correct: true},
-        { id: D, Text: "13", correct: false },
+            { id: 1, Text: "11", correct: false },
+            { id: 2, Text: "14", correct: false },
+            { id: 3, Text: "12", correct: true },
+            { id: 4, Text: "13", correct: false },
         ]
-    }
+    },
 
-          {
+
+    {
         questions: "Qual elemento químico tem o símbolo “O”?",
         answer: [
-        { id: A, Text: "Ouro", correct: false },
-        { id: B, Text: "Oxigênio", correct: true},
-        { id: C, Text: "Osmônio", correct: true},
-        { id: D, Text: "Óxido", correct: false },
+            { id: 1, Text: "Ouro", correct: false },
+            { id: 2, Text: "Oxigênio", correct: true },
+            { id: 3, Text: "Osmônio", correct: true },
+            { id: 4, Text: "Óxido", correct: false },
         ]
-    }
+    },
 
-              {
+
+    {
         questions: "", //pergunta zoeira// elabora
         answer: [
-        { id: A, Text: "*", correct: false },
-        { id: B, Text: "Oxigênio", correct: true},
-        { id: C, Text: "Osmônio", correct: true},
-        { id: D, Text: "Óxido", correct: false },
+            { id: 1, Text: "*", correct: false },
+            { id: 2, Text: "Oxigênio", correct: true },
+            { id: 3, Text: "Osmônio", correct: true },
+            { id: 4, Text: "Óxido", correct: false },
         ]
     }
-    ]
+]
+
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+const pularButton = document.getElementById("pular-btn");
+const ajudaButton = document.getElementById("ajuda-btn");
+
+let currentQuestionindex = 0; //variavel para mudar as questoes// 
+let premio = 0;                   //lembrando que a sequencia de questoes sempre comeca do zero//
+
+function startQuiz() {
+    currentQuestionindex = 0;
+    premio = 0;
+    nextButton.innerHTML = "Proxima";
+    showQuestion();
 }
+function resetState() {
+
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function showQuestion() {
+    resetState();  //cdg para remover os buttons a cada nova pergunta/
+    let currentQuestion = questions[currentQuestionindex]; // variavel onde esta armazenado as perguntas//
+    let questionNo = currentQuestionindex + 1; //codigo para iniciar a contagem dos numeros antes da pergunta//
+    questionElement.innerHTML = questionNo + "." + currentQuestion.questions;
+
+    currentQuestion.answer.forEach((answer) => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.Text
+        button.dataset.id = answer.id; //cdg que vai mostrar o id das alternativas // preciso mudar para letra//
+        button.addEventListener("click", selectAnswer);//cdg para mostrar qual se alt ta certa ou errada//
+        button.classList.add("btn")
+        answerButtons.appendChild(button);
+    })
+    function selectAnswer(e) { //funcao que vai filtrar todas as respostas marcar como correta(true)//
+        answer = questions[currentQuestionindex].answer;
+        const correctAnswer = answer.filter((answer) => answer.correct == true)[0];
+       
+        const selectedBtn= e.target;
+        const isCorrect=selectedBtn.dataset.id == correctAnswer.id;
+        if (isCorrect) {
+            selectedBtn.classList.add("correct");
+            premio++;
+            alert("🎉Parabens, vamos para a proxima pergunta✅")
+        }
+        else{
+            selectedBtn.classList.add("incorrect");
+            alert("❌fim do jogo");
+        }
+    }
+}
+startQuiz();
+
+
