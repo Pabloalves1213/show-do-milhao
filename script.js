8350c32f0b8e1a489b83b9e0a198273e4dbf29a7
Pablo Alves <pabloalves1213@gmail.com>
2025-07-29 22:12:03 -0300

    passando raiva

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const questions = [
 
     {
-        questions: "Qual é a capital do Brasil?",
+        question: "Qual é a capital do Brasil?",
         answer: [
             { id: 1, Text: "São Paulo", correct: false },
             { id: 2, Text: "Rio de janeiro", correct: false },
@@ -15,7 +15,7 @@ const questions = [
 
 
     {
-        questions: "Quem pintou a Mona Lisa?",
+        question: "Quem pintou a Mona Lisa?",
         answer: [
             { id: 1, Text: "Vincent van Gogh", correct: false },
             { id: 2, Text: "Leonardo da Vinci ", correct: true },
@@ -26,7 +26,7 @@ const questions = [
 
 
     {
-        questions: "Qual planeta é conhecido como o Planeta Vermelho",
+        question: "Qual planeta é conhecido como o Planeta Vermelho",
         answer: [
             { id: 1, Text: "Júpiter", correct: false },
             { id: 2, Text: "Marte", correct: true },
@@ -38,7 +38,7 @@ const questions = [
 
 
     {
-        questions: "Qual tag HTML é usada para criar um parágrafo?",
+        question: "Qual tag HTML é usada para criar um parágrafo?",
         answer: [
             { id: 1, Text: "<div>", correct: false },
             { id: 2, Text: "<span>", correct: false },
@@ -50,7 +50,7 @@ const questions = [
 
 
     {
-        questions: "Qual desses animais é um mamífero?",
+        question: "Qual desses animais é um mamífero?",
         answer: [
             { id: 1, Text: "Jacaré", correct: false },
             { id: 2, Text: "Tartaruga", correct: false },
@@ -61,18 +61,18 @@ const questions = [
 
 
     {
-        questions: "Quanto é 7 x 8?",
+        question: "Quanto é 7 x 8?",
         answer: [
             { id: 1, Text: "56", correct: true },
             { id: 2, Text: "64", correct: false },
             { id: 3, Text: "49", correct: false },
             { id: 4, Text: "58", correct: false },
         ]
-    },
+    },                //pergunta vitor pq a virgula influencia no codigo//
 
 
     {
-        questions: "Qual dessas palavras é um verbo?",
+        question: "Qual dessas palavras é um verbo?",
         answer: [
             { id: 1, Text: "Casa", correct: false },
             { id: 2, Text: "Rapído", correct: false },
@@ -83,7 +83,7 @@ const questions = [
 
 
     {
-        questions: "Quanto é a raiz quadrada de 144?",
+        question: "Quanto é a raiz quadrada de 144?",
         answer: [
             { id: 1, Text: "11", correct: false },
             { id: 2, Text: "14", correct: false },
@@ -94,7 +94,7 @@ const questions = [
 
 
     {
-        questions: "Qual elemento químico tem o símbolo “O”?",
+        question: "Qual elemento químico tem o símbolo “O”?",
         answer: [
             { id: 1, Text: "Ouro", correct: false },
             { id: 2, Text: "Oxigênio", correct: true },
@@ -105,7 +105,7 @@ const questions = [
 
 
     {
-        questions: "", //pergunta zoeira// elabora
+        question: "", //pergunta zoeira// elabora
         answer: [
             { id: 1, Text: "*", correct: false },
             { id: 2, Text: "Oxigênio", correct: true },
@@ -127,7 +127,7 @@ let premio = 0;                   //lembrando que a sequencia de questoes sempre
 
 function startQuiz() {
     currentQuestionindex = 0;
-    premio = 0;
+    score= 0;
     nextButton.innerHTML = "Proxima";
     showQuestion();
 }
@@ -137,12 +137,14 @@ function resetState() {
         answerButtons.removeChild(answerButtons.firstChild);
     }
 }
+ 
+
 
 function showQuestion() {
     resetState();  //cdg para remover os buttons a cada nova pergunta/
     let currentQuestion = questions[currentQuestionindex]; // variavel onde esta armazenado as perguntas//
     let questionNo = currentQuestionindex + 1; //codigo para iniciar a contagem dos numeros antes da pergunta//
-    questionElement.innerHTML = questionNo + "." + currentQuestion.questions;
+    questionElement.innerHTML = questionNo + "." + currentQuestion.question;
 
     currentQuestion.answer.forEach((answer) => {
         const button = document.createElement("button");
@@ -155,19 +157,45 @@ function showQuestion() {
     function selectAnswer(e) { //funcao que vai filtrar todas as respostas marcar como correta(true)//
         answer = questions[currentQuestionindex].answer;
         const correctAnswer = answer.filter((answer) => answer.correct == true)[0];
-       
-        const selectedBtn= e.target;
-        const isCorrect=selectedBtn.dataset.id == correctAnswer.id;
+
+        const selectedBtn = e.target;
+        const isCorrect = selectedBtn.dataset.id == correctAnswer.id;
         if (isCorrect) {
             selectedBtn.classList.add("correct");
-            premio++;
-            alert("🎉Parabens, vamos para a proxima pergunta✅")
+            score++;
+            alert("🎉Certa reposta✅")
         }
-        else{
+        else {
             selectedBtn.classList.add("incorrect");
             alert("❌fim do jogo");
         }
+        Array.from(answerButtons.children).forEach((button) => {
+            button.disabled = true;  //cdg para evitar que participante click em duas ou mais alternativas//
+        });
+
+        function showScore() {
+            resetState();
+            questionElement.innerHTML = `voce ganhou ${premio} de ${questionElement.length}!`;
+            nextButton.innerHTML = "play Again"
+        }
+                 //erro esta aqui//
+        function handleNextButton() {  // cdg para nao dar erro nas perguntas seguintes//
+            currentQuestionindex++;
+            if (currentQuestionindex < questions.length) {
+                showScore(); //vai dar um reset nas opcoes puxando a proxima pergunta e alternativas//   
+            }
+            else {
+                premioQuestion();   // codigo de teste!! ver se vai entregar o resultado no final do jogo//
+            }
+        };
     }
+    nextButton.addEventListener("click", () => {
+        if (currentQuestionindex < questions.length) {
+            handleNextButton();
+        } else {
+            startQuiz();
+        }
+    })
 }
 startQuiz();
 
