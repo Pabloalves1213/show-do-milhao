522f0d9701f31015c751feb8f6219f0be4552835
Pablo Alves <pabloalves1213@gmail.com>
2025-07-31 21:35:35 -0300

    atuliazacao 31/07

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
 
 const questions = [
-
     {
         question: "Qual é a capital do Brasil?",
         answer: [
@@ -11,9 +10,6 @@ const questions = [
             { id: 4, Text: "Salvador", correct: false },
         ]
     },
-
-
-
     {
         question: "Quem pintou a Mona Lisa?",
         answer: [
@@ -23,8 +19,6 @@ const questions = [
             { id: 4, Text: "Michelangelo", correct: false },
         ]
     },
-
-
     {
         question: "Qual planeta é conhecido como o Planeta Vermelho",
         answer: [
@@ -34,21 +28,15 @@ const questions = [
             { id: 4, Text: "Saturno", correct: false },
         ]
     },
-
-
-
     {
         question: "Qual tag HTML é usada para criar um parágrafo?",
         answer: [
-            { id: 1, Text: "<div>", correct: false },
-            { id: 2, Text: "<span>", correct: false },
-            { id: 3, Text: "<p>", correct: true },
-            { id: 4, Text: "<text>", correct: false },
+            { id: 1, Text: "div", correct: false },
+            { id: 2, Text: "span", correct: false },
+            { id: 3, Text: "p", correct: true },
+            { id: 4, Text: "text", correct: false },
         ]
     },
-
-
-
     {
         question: "Qual desses animais é um mamífero?",
         answer: [
@@ -58,8 +46,6 @@ const questions = [
             { id: 4, Text: "Pinguim", correct: false },
         ]
     },
-
-
     {
         question: "Quanto é 7 x 8?",
         answer: [
@@ -69,8 +55,6 @@ const questions = [
             { id: 4, Text: "58", correct: false },
         ]
     },                //pergunta vitor pq a virgula influencia no codigo//
-
-
     {
         question: "Qual dessas palavras é um verbo?",
         answer: [
@@ -80,8 +64,6 @@ const questions = [
             { id: 4, Text: "Amarelo", correct: false },
         ]
     },
-
-
     {
         question: "Quanto é a raiz quadrada de 144?",
         answer: [
@@ -91,8 +73,6 @@ const questions = [
             { id: 4, Text: "13", correct: false },
         ]
     },
-
-
     {
         question: "Qual elemento químico tem o símbolo “O”?",
         answer: [
@@ -102,8 +82,6 @@ const questions = [
             { id: 4, Text: "Óxido", correct: false },
         ]
     },
-
-
     {
         question: "", //pergunta zoeira// elabora
         answer: [
@@ -119,15 +97,15 @@ const questions = [
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
-const pularButton = document.getElementById("pular-btn");
+const pararButton = document.getElementById("parar-btn");
 const ajudaButton = document.getElementById("ajuda-btn");
 
-let currentQuestionindex = 0; //variavel para mudar as questoes// 
+let currentQuestionindex = 0;     //variavel para mudar as questoes// 
 let premio = 0;                   //lembrando que a sequencia de questoes sempre comeca do zero//
 
 function startQuiz() {
     currentQuestionindex = 0;
-    score= 0;
+    score = 0;
     nextButton.innerHTML = "Proxima";
     showQuestion();
 }
@@ -137,7 +115,7 @@ function resetState() {
         answerButtons.removeChild(answerButtons.firstChild);
     }
 }
- 
+
 
 
 function showQuestion() {
@@ -147,12 +125,12 @@ function showQuestion() {
     questionElement.innerHTML = questionNo + "." + currentQuestion.question;
 
     currentQuestion.answer.forEach((answer) => {
-        const button = document.createElement("button");
-        button.innerHTML = answer.Text
+        const button = document.createElement("button"); //mostra as alternativas//
+        button.innerHTML = answer.Text //mostrar o text dentro da alternativa//
         button.dataset.id = answer.id; //cdg que vai mostrar o id das alternativas // preciso mudar para letra//
-        button.addEventListener("click", selectAnswer);//cdg para mostrar qual se alt ta certa ou errada//
-        button.classList.add("btn")
-        answerButtons.appendChild(button);
+        button.addEventListener("click", selectAnswer);//cdg para mostrar as respotas das alternativas certa ou errada//
+        button.classList.add("btn") // puxa css dos button//
+        answerButtons.appendChild(button); //fez a mesma funcao de mostrar as opcoes de alt disponivel//
     })
     function selectAnswer(e) { //funcao que vai filtrar todas as respostas marcar como correta(true)//
         answer = questions[currentQuestionindex].answer;
@@ -163,31 +141,38 @@ function showQuestion() {
         if (isCorrect) {
             selectedBtn.classList.add("correct");
             score++;
-            alert("🎉Certa reposta✅")
+           // alert("🎉Certa reposta✅")// lembra de mudar
         }
         else {
             selectedBtn.classList.add("incorrect");
-            alert("❌fim do jogo");
+           // alert("❌fim do jogo");// lembra de mudar
         }
         Array.from(answerButtons.children).forEach((button) => {
             button.disabled = true;  //cdg para evitar que participante click em duas ou mais alternativas//
         });
 
-        function showScore() {
-            resetState();
-            questionElement.innerHTML = `voce ganhou ${premio} de ${questionElement.length}!`;
-            nextButton.innerHTML = "play Again"
+    }
+
+    //erro esta aqui//
+    function handleNextButton() {  // cdg para nao dar erro nas perguntas seguintes//
+        console.log("pergunta atual: " + currentQuestionindex);
+        currentQuestionindex++;    //++ soma // 
+        console.log("testando proxima");
+
+        if (currentQuestionindex < questions.length) {
+            showQuestion();
         }
-                 //erro esta aqui//
-        function handleNextButton() {  // cdg para nao dar erro nas perguntas seguintes//
-            currentQuestionindex++;
-            if (currentQuestionindex < questions.length) {
-                showScore(); //vai dar um reset nas opcoes puxando a proxima pergunta e alternativas//   
-            }
-            else {
-                premioQuestion();   // codigo de teste!! ver se vai entregar o resultado no final do jogo//
-            }
-        };
+        else {
+            console.log("chamou premioQuestion");
+
+            premioQuestion();   // codigo de teste!! ver se vai entregar o resultado no final do jogo//
+        }
+    };
+
+    function showScore() {
+        resetState();
+        questionElement.innerHTML = `voce ganhou ${premio} de ${questionElement.length}!`;
+        nextButton.innerHTML = "play Again"
     }
     nextButton.addEventListener("click", () => {
         if (currentQuestionindex < questions.length) {
@@ -197,6 +182,9 @@ function showQuestion() {
         }
     })
 }
+
 startQuiz();
 
+
+//erro pode ser o eventlistener com onclick// observar
 
